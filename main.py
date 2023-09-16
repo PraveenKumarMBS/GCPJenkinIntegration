3780162073763f6f31687e92fd4bbd19ce48892b
PraveenKumarMBS <39936932+PraveenKumarMBS@users.noreply.github.com>
2023-09-16 10:51:17 +0530

    Update main.py
    GCP Integeration

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def hello():
     Returns:
         A string with the words 'Hello World!'.
     """
-    return "Hello World Jenkin_GCP Integration!"
+    return "Hello World Jenkin_GCP Integration_thru jenkins checkout!"
 
 
 if __name__ == "__main__":
